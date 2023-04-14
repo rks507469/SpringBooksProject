0691ed1e0b099d4e581a73da4bcca267dd4b126c
Mohd Aslam <mohd.aslam@riaadvisory.com>
2023-04-15 02:50:14 +0530

    Added equals, hashCode and toString methods to Author and Book Entities

--- src/main/java/guru/springframework/booksproject/domain/Author.java
+++ src/main/java/guru/springframework/booksproject/domain/Author.java
@@ -3,6 +3,7 @@ package guru.springframework.booksproject.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -54,5 +55,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
